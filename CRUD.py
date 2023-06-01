20c06830ce76177741abb127ecfb282f45af9e02
KNSVita <100169987+KNSVita@users.noreply.github.com>
2023-06-01 08:48:15 -0300

    Update CRUD.py

--- CRUD.py
+++ CRUD.py
@@ -1,134 +1,116 @@
-lista = []
-tipo_jogos = []
+NOME_ARQUIVO = "biblioteca_jogos.txt"
 
-#MENU INICIO
-while True:
-
-    print("\nBem Vindo a sua bliblioteca de jogos!!!\n")
-    print("Abaixo estão listadas algumas funções. Comece adicionando alguns jogos a sua bliblioteca basta digitar 1\n")
-
-    print("[1] Inserir game")
-    print("[2] Consultar games")
-    print("[3] Alterar game")
-    print("[4] Excluir game")
-    print("[5] Salvar bliblioteca (Só utilizar quand já tiver adicionado jogos a sua bliblioteca.)")
-    print("[6] Importar bliblioteca (Só utilize essa opção se já tiver salvo sua bliblioteca.)")
+#MENU
+def exibir_menu():
+    print("\nBem-vindo à sua biblioteca de jogos!!!\n")
+    print("Abaixo estão listadas algumas funções. Comece adicionando alguns jogos à sua biblioteca.")
+    print("[1] Inserir jogo")
+    print("[2] Consultar jogos")
+    print("[3] Alterar jogo")
+    print("[4] Excluir jogo")
+    print("[5] Salvar biblioteca (utilize quando já tiver adicionado jogos à sua biblioteca)")
+    print("[6] Importar biblioteca (utilize essa opção se já tiver salvo sua biblioteca)")
     print("[7] Sair\n")
 
+def inserir_jogo(biblioteca):
+    nome = input("\nInsira o nome do jogo: ")
+    tipo = input("Insira o tipo do jogo: ")
+    jogo = {"nome": nome, "tipo": tipo}
+    biblioteca.append(jogo)
+    print("Jogo inserido com sucesso!")
+    exibir_jogos(biblioteca)
 
-    resposta = int(input("Digite a opção desejada : "))
-#MENU FIM
-
-#INSERT
-    if resposta == 1:
-        insira = str(input("\nO insira um game ?\n"))
-        lista.append(insira)
-        insira_2 = str(input("\nQual o tipo do game ?\n"))
-        tipo_jogos.append(insira_2)
-        print("\nGame inserido com sucesso!!!\n")
-        print('\nJogo :',lista)
-        print('Tipo :',tipo_jogos)
-
-#Select
-    elif resposta == 2:
-        consultar = str(input("\nQual game quer buscar ?\n"))
-        if consultar == "":
-            print("Vc n digitou nada para fazer a pesquisa")
-            exit()
-        if consultar in lista:
-            print("O jogo está na lista\n")
-            j_pos = lista.index(consultar)
-            print(lista[j_pos])
-            print(tipo_jogos[j_pos],'\n')
-        else:
-            consultarn = consultar+'\n'
-            if consultarn in lista:
-                print("O jogo está na lista\n")
-                j_pos = lista.index(consultarn)
-                print(lista[j_pos])
-                print(tipo_jogos[j_pos],'\n')
-            else:
-                print("\nO item não esta na lista. Gostaria de adicionar ?\n")
-                print("[1] Sim")
-                print("[2] Não\n")
-                cond_consu = int(input("Selecione : "))
-                if cond_consu == 1:
-                    lista.append(consultar)
-                    select_tipo= str(input("\nEscreva o tipo desse jogo : ")) 
-                    tipo_jogos.append(select_tipo)
-                    print("\nJogo adicionado\n")
-                elif cond_consu == 2:
-                    print("\nVoltando para o menu\n")
-
-
-#Alterar
-    elif resposta == 3:
-        print(lista)
-        antigo = str(input("\nEscreva um jogo da lista acima para fazer a substituição : \n"))
-        if antigo == "":
-            print("Vc n digitou nada para fazer a pesquisa")
-            exit()
-        if antigo in lista:
-            g_pos = lista.index(antigo)
-            novo = str(input("Digite o jogo que vai substituir o elemento que digitou anteriormente : "))
-            lista[g_pos] = novo
-            novo_tipo = str(input("Digite o novo tipo do seu jogo : "))
-            tipo_jogos[g_pos] = novo_tipo
-        else :
-            antigon = antigo + '\n'
-            g_pos = lista.index(antigon)
-            novo = str(input("Digite o jogo que vai substituir o elemento que digitou anteriormente : "))
-            lista[g_pos] = novo
-            novo_tipo = str(input("Digite o novo tipo do seu jogo : "))
-            tipo_jogos[g_pos] = novo_tipo
-        print(lista)
-        print(tipo_jogos)
-
-
-#Delete
-    elif resposta == 4:
-        print(lista,'\n')
-        remover = str(input("Digite o game para ser removido (NÃO ESCREVA : '\-n') : \n"))
-        if remover == "":
-            print("Vc n digitou nada para fazer a pesquisa")
-            exit()
-        if remover in lista:
-            r_pos = lista.index(remover)
-            lista.remove(remover)
-            t_rem = tipo_jogos[r_pos]
-            tipo_jogos.remove(t_rem)
-            print("Itens removidos")
-        else:
-            removern = remover+'\n'
-            r_pos = lista.index(removern)
-            lista.remove(removern)
-            t_rem = tipo_jogos[r_pos]
-            tipo_jogos.remove(t_rem)
-            print("Itens removidos")
-            print(lista)
-
-#Gravar arquivo
-    elif resposta == 5:
-        arquivo = open("texto_nome.txt","a")
-        arquivo_tipo = open("texto_tipo.txt","a")
-        arquivo.writelines(line + "\n" for line in lista)
-        arquivo_tipo.writelines(line_t + "\n" for line_t in tipo_jogos)
-        print("Bliblioteca salva!! (Ao fechar o programa para importar basta digitar a opção 6)")
-
-#Importar arquivo
-    elif resposta == 6:
-        arquivo = open("texto_nome.txt","r")
-        arquivo_tipo = open("texto_tipo.txt","r")
-        lista = arquivo.readlines()
-        tipo_jogos = arquivo_tipo.readlines()
-        print("\n",lista)
-        print(tipo_jogos)
-        print("Bliblioteca importada!!")
-
-
-#Exit
-    elif resposta == 7:
-        exit()
-
+def consultar_jogos(biblioteca):
+    consulta = input("\nQual jogo você deseja buscar? ")
+    encontrados = []
+    for jogo in biblioteca:
+        if jogo["nome"] == consulta:
+            encontrados.append(jogo)
+    if encontrados:
+        print("Jogo(s) encontrado(s):")
+        for jogo in encontrados:
+            print(f"Nome: {jogo['nome']}, Tipo: {jogo['tipo']}")
     else:
-        print("Não existe essa opção, te retornando para o menu.")
+        print("Jogo não encontrado.")
+
+def alterar_jogo(biblioteca):
+    nome_antigo = input("\nDigite o nome do jogo que deseja alterar: ")
+    jogo_encontrado = None
+    for jogo in biblioteca:
+        if jogo["nome"] == nome_antigo:
+            jogo_encontrado = jogo
+            break
+    if jogo_encontrado:
+        novo_nome = input("Digite o novo nome do jogo: ")
+        novo_tipo = input("Digite o novo tipo do jogo: ")
+        jogo_encontrado["nome"] = novo_nome
+        jogo_encontrado["tipo"] = novo_tipo
+        print("Jogo alterado com sucesso!")
+        exibir_jogos(biblioteca)
+    else:
+        print("Jogo não encontrado.")
+
+def excluir_jogo(biblioteca):
+    nome = input("\nDigite o nome do jogo que deseja excluir: ")
+    for jogo in biblioteca:
+        if jogo["nome"] == nome:
+            biblioteca.remove(jogo)
+            print("Jogo removido com sucesso!")
+            exibir_jogos(biblioteca)
+            return
+    print("Jogo não encontrado.")
+
+def exibir_jogos(biblioteca):
+    print("\nBiblioteca de Jogos:")
+    for jogo in biblioteca:
+        print(f"Nome: {jogo['nome']}, Tipo: {jogo['tipo']}")
+
+def salvar_biblioteca(biblioteca):
+    with open(NOME_ARQUIVO, "w") as arquivo:
+        for jogo in biblioteca:
+            arquivo.write(f"{jogo['nome']},{jogo['tipo']}\n")
+    print("Biblioteca salva!")
+
+def importar_biblioteca():
+    biblioteca = []
+    try:
+        with open(NOME_ARQUIVO, "r") as arquivo:
+            for linha in arquivo:
+                nome, tipo = linha.strip().split(",")
+                jogo = {"nome": nome, "tipo": tipo}
+                biblioteca.append(jogo)
+        print("Biblioteca importada!")
+        exibir_jogos(biblioteca)
+        return biblioteca
+    except FileNotFoundError:
+        print("Nenhum arquivo de biblioteca encontrado.")
+        return []
+
+def main():
+    biblioteca = importar_biblioteca()
+
+    while True:
+        exibir_menu()
+        opcao = input("Digite a opção desejada: ")
+
+        if opcao == "1":
+            inserir_jogo(biblioteca)
+        elif opcao == "2":
+            consultar_jogos(biblioteca)
+        elif opcao == "3":
+            alterar_jogo(biblioteca)
+        elif opcao == "4":
+            excluir_jogo(biblioteca)
+        elif opcao == "5":
+            salvar_biblioteca(biblioteca)
+        elif opcao == "6":
+            biblioteca = importar_biblioteca()
+        elif opcao == "7":
+            break
+        else:
+            print("Opção inválida!")
+
+    print("Saindo do programa...")
+
+if __name__ == "__main__":
+    main()
